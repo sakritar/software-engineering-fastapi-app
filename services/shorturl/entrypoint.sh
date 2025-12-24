8211a040ab89a793716650b757cd3e7bf5dc717d
leskov-m <leskov760@gmail.com>
2025-12-24 17:48:41 +0500

    feat: environment

--- services/shorturl/entrypoint.sh
+++ services/shorturl/entrypoint.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
 mkdir -p /app/data
 alembic upgrade head
-uvicorn main:app --host=0.0.0.0 --port=8001
-
+uvicorn main:app --host=0.0.0.0 --port=8001 --reload
--- services/todo/entrypoint.sh
+++ services/todo/entrypoint.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
 mkdir -p /app/data
 alembic upgrade head
-uvicorn main:app --host=0.0.0.0 --port=8000
-
+uvicorn main:app --host=0.0.0.0 --port=8000 --reload
